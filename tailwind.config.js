9f1184a5d292b97fc52501b05d7bf1807e502f2a
zisan5910 <zisan5910@gmail.com>
2025-07-17 00:49:49 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'UniConverter',
+        short_name: 'UniConverter',
+        description: 'Universal Unit Converter',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'https://i.postimg.cc/wMPW5PVM/20250717-004001-0000.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://i.postimg.cc/wMPW5PVM/20250717-004001-0000.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
-  theme: {
-    extend: {},
+  server: {
+    port: 8080
   },
-  plugins: [],
-}
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+})
